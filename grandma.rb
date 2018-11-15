1a8c83a6dbf6aef7b2611df832b1d6bf4575a7ac
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-15 23:56:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(speak_to_grandma="Hi!")
-   speak_to_grandma      
+   speak_to_grandma     
+   if speak_to_grandma == "Hi!"
+     puts "Hello!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
