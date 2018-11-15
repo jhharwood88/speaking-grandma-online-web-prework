01f5093b3c33971fb7d7b1c072db674a1b08e168
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-15 23:37:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,17 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(speak_to_grandma)
- puts speak_to_grandma
+def speak_to_grandma(speak_to_grandma="Hi!")
+  puts speak_to_grandma
+
+  if speak_to_grandma == ("I LOVE YOU GRANDMA!")
+    puts "I LOVE YOU TOO PUMPKIN!"
+
+  elsif speak_to_grandma == #{speak_to_grandma}.upcase
+    puts "NO, NOT SINCE 1938!"
+
+  else speak_to_grandma == #{speak_to_grandma}.downcase || #{speak_to_grandma}.captalize
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
