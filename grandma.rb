69fc6b55668842ca868170c2d3c005657d571585
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-15 23:38:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,8 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(speak_to_grandma="Hi!")
-  puts speak_to_grandma
+dog = "hungry"
 
-  
-end
+if dog = 
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
